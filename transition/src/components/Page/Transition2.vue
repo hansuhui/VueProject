56902fe9f439d7afb304d3eff2dbdad73b4046ac
HanSuHui <poixgks@naver.com>
2018-02-26 16:01:43 +0900

    transision

---
+++ transition/src/components/Page/Transition2.vue
@@ -0,0 +1,122 @@
+
+<template>
+<div>
+<Header></Header>
+<div class="wrapper">
+    <section id="breadcrumb" class="hoc clear">
+      <!-- ################################################################################################ -->
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Ipsum</a></li>
+        <li><a href="#">Dolor</a></li>
+      </ul>
+      <!-- ################################################################################################ -->
+      <h6 class="heading">Transition2</h6>
+      <!-- ################################################################################################ -->
+    </section>
+  </div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear">
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="content">
+      <!-- ################################################################################################ -->
+      <div id="gallery">
+        <figure>
+          <header class="heading">Gallery Title Goes Here</header>
+          <ul class="nospace clear">
+              <li v-for='(list,index) in imgList' v-bind:key="index" class="one_quarter" :class="{ first:index % 4 == 0}">
+                <transition name="slide-fade">
+                <a style="cursor: pointer;" v-show="list.isShow" v-on:click="test(list)"><img src="@/assets/images/demo/gallery/01.png" alt=""></a>
+                </transition>
+              </li>
+          </ul>
+          <figcaption>Gallery Description Goes Here</figcaption>
+        </figure>
+      </div>
+      <!-- ################################################################################################ -->
+      <!-- ################################################################################################ -->
+      <nav class="pagination">
+        <ul>
+          <li><a href="#">&laquo; Previous</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><strong>&hellip;</strong></li>
+          <li><a href="#">6</a></li>
+          <li class="current"><strong>7</strong></li>
+          <li><a href="#">8</a></li>
+          <li><a href="#">9</a></li>
+          <li><strong>&hellip;</strong></li>
+          <li><a href="#">14</a></li>
+          <li><a href="#">15</a></li>
+          <li><a href="#">Next &raquo;</a></li>
+        </ul>
+      </nav>
+      <!-- ################################################################################################ -->
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<Footer></Footer>
+</div>
+</template>
+<script>
+
+import Header from '@/components/Shared/_Header'
+import Footer from '@/components/Shared/_Footer'
+
+export default {
+  name: 'Transition2',
+  components: { Header, Footer },
+  data () {
+    return {
+      msg: '',
+      imgList: GetList()
+    }
+  },
+  props: ['name'],
+  created: function () {
+    console.log(this.name)
+  },
+  methods: {
+    test: function (list) {
+      list.isShow = !list.isShow
+    }
+  }
+}
+
+function GetList () {
+  var result = new Array(20)
+  for (var i = 0; i < 20; i++) {
+    result[i] = {isShow: true}
+  }
+  return result
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* 애니메이션 진입 및 진출은 다른 지속 시간 및  */
+/* 타이밍 기능을 사용할 수 있습니다. */
+.slide-fade-enter-active {
+  transition: all 1s ease;
+}
+.slide-fade-leave-active {
+  transition: all 1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(50px);
+  opacity: 0;
+}
+</style>
---
+++ transition/src/components/Shared/_Header.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="bgded overlay bg1" style="background-image:url('@/assets/images/demo/backgrounds/01.png');">
+  <!-- ################################################################################################ -->
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear">
+      <!-- ################################################################################################ -->
+      <div class="fl_left">
+        <ul class="nospace">
+          <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Register</a></li>
+        </ul>
+      </div>
+      <div class="fl_right">
+        <ul class="nospace">
+          <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
+          <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
+        </ul>
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear">
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+        <h1><router-link :to="{ name: 'HomeIndex', params: {name : 'HomeIndex' } }">Bronea</router-link></h1>
+      </div>
+      <!-- ################################################################################################ -->
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li :class="{active : activeName =='HomeIndex'}"><router-link :to="{ name: 'HomeIndex', params: {name : 'HomeIndex' } }">Home</router-link></li>
+          <li :class="{active : is_Page}"><a class="drop" href="#">Pages</a>
+            <ul>
+              <li :class="{active : activeName =='Transition'}"><router-link :to="{ name: 'Transition', params: { name : 'Transition' } }">Transition</router-link></li>
+              <li :class="{active : activeName =='Transition2'}"><router-link :to="{ name: 'Transition2', params: { name : 'Transition2' } }">Transition2</router-link></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Dropdown</a>
+            <ul>
+              <li><a href="#">Level 2</a></li>
+              <li><a class="drop" href="#">Level 2 + Drop</a>
+                <ul>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Level 2</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Link Text</a></li>
+          <li><a href="#">Link Text</a></li>
+        </ul>
+      </nav>
+      <!-- ################################################################################################ -->
+    </header>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'LayoutHeader',
+  data () {
+    return {
+      activeName: '',
+      is_Page: false
+    }
+  },
+  created: function () {
+    this.activeName = this._routerRoot._router.history.current.name
+    if (this.activeName !== 'HomeIndex') {
+      this.is_Page = true
+    } else {
+      this.is_Page = false
+    }
+  }
+}
+</script>
+
+<style src="@/assets/layout/styles/layout.css"></style>
